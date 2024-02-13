4965fd91236c869cda2cd2fa5800402c72e177e5
haya73 <kmgo8810@gmail.com>
2024-02-13 22:24:58 +0900

    02

--- app/controllers/workers_controller.rb
+++ app/controllers/workers_controller.rb
@@ -1,14 +1,28 @@
 class WorkersController < ApplicationController
-
+  before_action :authenticate_user!, except: [:index, :show, :destroy]
 
   def index
+    @workers = Worker.includes(:user)
   end
 
   def new
-
+    @worker = Worker.new
   end
 
   def create
+    binding.pry
+    @worker = Worker.new(worker_params)
+    if @worker.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
 
+
+
+  private
+  def worker_params
+    params.require(:worker).permit(:title, :catch_copy, :pt_name, :image).merge(user_id: current_user.id)
   end
 end
--- app/models/worker.rb
+++ app/models/worker.rb
@@ -1,5 +1,6 @@
 class Worker < ApplicationRecord
   belongs_to :user
+  #has_one_attached :image
   #has_many :comments, dependent: :destroy
 
   validates :title,       presence: true
